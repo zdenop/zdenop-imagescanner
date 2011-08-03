177f25485398261fc321d595673bf6271f5e8eae
Sergio Campos <seocam@seocam.com>
2011-08-03 14:43:09 -0300

    Improving documentation

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+REQUIREMENTS = [
+    'autoconnect', 
+    'importlib',
+    'PIL',
+]
+
+# Required only by Posix:
+if os.name == 'posix': REQUIREMENTS.append('pysane')
+
+setup(name='imagescanner',
+      version='0.9',
+      description='Multi-platform Python library to access scanner devices.',
+      author='Sergio Oliveira',
+      author_email='seocam@seocam.com',
+      url='http://code.google.com/p/imagescanner/',
+      packages=find_packages(),
+      package_data={'': ['*.tiff']},
+      install_requires=REQUIREMENTS,
+     )
+
