01cecb41dfad4d8514597c49bd36f1005b2374f6
Sergio Oliveira <seocam@seocam.com>
2011-08-04 20:32:21 -0300

    fixing identation

--- imagescanner/backends/osx/_scanning.m
+++ imagescanner/backends/osx/_scanning.m
@@ -5,61 +5,66 @@
 
 const char* getStringValue(NSDictionary* dict, NSString* key)
 {
-	return [[dict objectForKey:key] UTF8String];
+    return [[dict objectForKey:key] UTF8String];
 }
 
 
 unsigned long int getNumericValue(NSDictionary* dict, NSString* key)
 {
-	return [[dict objectForKey:key] unsignedLongValue];
+    return [[dict objectForKey:key] unsignedLongValue];
 }
 
 
 PyObject* getListOfStringsValue(NSDictionary* dict, NSString* key)
 {
-	NSArray *array = [dict objectForKey:key];
-	Py_ssize_t i;
-	Py_ssize_t len = [array count];
-	PyObject *list = PyList_New(len);	
-	for (i = 0; i < len; ++i) {
-		PyObject *item = PyString_FromString([[array objectAtIndex:i] UTF8String]);
-		NSCAssert(item != NULL, @"Can't add NULL item to Python List");
-		PyList_SetItem(list, i, item);
-	}
-	return list;
+    NSArray *array = [dict objectForKey:key];
+    Py_ssize_t i;
+    Py_ssize_t len = [array count];
+    PyObject *list = PyList_New(len);    
+    for (i = 0; i < len; ++i) {
+        PyObject *item = PyString_FromString([[array objectAtIndex:i] UTF8String]);
+        NSCAssert(item != NULL, @"Can't add NULL item to Python List");
+        PyList_SetItem(list, i, item);
+    }
+    return list;
 }
 
 
 PyObject* devicePropertyDictToPyObject(NSDictionary* devicePropertyDict)
-{		
-	return Py_BuildValue("{s:s,s:s,s:s,s:s,s:s,s:s,s:n,s:n,s:n,s:O}",
-						 "name", getStringValue(devicePropertyDict, @"ifil"),
-						 "serial_number",  getStringValue(devicePropertyDict, @"ICADeviceSerialNumberString"),
-						 "type", getStringValue(devicePropertyDict, @"ICADeviceTypeKey"),
-						 "user_custom_name", getStringValue(devicePropertyDict, @"ICAUserAssignedDeviceNameKey"),
-						 "session_status",  getStringValue(devicePropertyDict, @"scannerSessionStatus"),
-						 "uuid", getStringValue(devicePropertyDict, @"UUIDString"),
-						 "id", getNumericValue(devicePropertyDict, @"icao"),
-						 "idProduct", getNumericValue(devicePropertyDict, @"idProduct"),
-						 "idVendor", getNumericValue(devicePropertyDict, @"idVendor"),
-						 "resolutions", getListOfStringsValue(devicePropertyDict, @"preferred resolutions"));
+{        
+    return Py_BuildValue("{s:s,s:s,s:s,s:s,s:s,s:s,s:n,s:n,s:n,s:O}",
+        "name", getStringValue(devicePropertyDict, @"ifil"),
+        "serial_number",  getStringValue(devicePropertyDict, 
+                                         @"ICADeviceSerialNumberString"),
+        "type", getStringValue(devicePropertyDict, @"ICADeviceTypeKey"),
+        "user_custom_name", getStringValue(devicePropertyDict,
+                                           @"ICAUserAssignedDeviceNameKey"),
+        "session_status",  getStringValue(devicePropertyDict, 
+                                          @"scannerSessionStatus"),
+        "uuid", getStringValue(devicePropertyDict, @"UUIDString"),
+        "id", getNumericValue(devicePropertyDict, @"icao"),
+        "idProduct", getNumericValue(devicePropertyDict, @"idProduct"),
+        "idVendor", getNumericValue(devicePropertyDict, @"idVendor"),
+        "resolutions", getListOfStringsValue(devicePropertyDict, 
+                                             @"preferred resolutions")
+    );
 }
 
 
 PyObject* deviceListToPyObject(NSArray *devices)
 {
-	// Cast the devices List to pyobject
-	Py_ssize_t i;
-	Py_ssize_t len = [devices count];
-	PyObject *py_devices = PyList_New(len);
-	
-	for (i = 0; i < len; ++i) {
-		PyObject *item = devicePropertyDictToPyObject([devices objectAtIndex:i]);
-		NSCAssert(item != NULL, @"Can't add NULL item to Python List");
-		PyList_SetItem(py_devices, i, item);
-	}
-	
-	return py_devices;
+    // Cast the devices List to pyobject
+    Py_ssize_t i;
+    Py_ssize_t len = [devices count];
+    PyObject *py_devices = PyList_New(len);
+    
+    for (i = 0; i < len; ++i) {
+        PyObject *item = devicePropertyDictToPyObject([devices objectAtIndex:i]);
+        NSCAssert(item != NULL, @"Can't add NULL item to Python List");
+        PyList_SetItem(py_devices, i, item);
+    }
+    
+    return py_devices;
 }
 
 
@@ -70,41 +75,33 @@ PyObject* getDeviceList(PyObject *self, PyObject *args)
     NSDictionary* deviceListPropDict = NULL;
     NSArray* devices;
     OSErr err;
-	
+    
     // get the device list pb
     err = ICAGetDeviceList(&deviceList_pb, NULL);
     if (err != noErr) return NULL;
-	
+    
     // load the device list property dict pb
     deviceListPropDict_pb.object = deviceList_pb.object;
     deviceListPropDict_pb.theDict = (CFDictionaryRef*)(&deviceListPropDict);
     err = ICACopyObjectPropertyDictionary( &deviceListPropDict_pb, NULL );
     if (err != noErr) return NULL;
-	
+    
     // get the device list
     devices = [deviceListPropDict objectForKey:@"devices"];
 
-	return deviceListToPyObject(devices);	
+    return deviceListToPyObject(devices);    
 }
 
 static PyMethodDef scanningMethods[] = {
-	{"getDeviceList", getDeviceList, METH_VARARGS, "TODO"},
-	{NULL, NULL, 0, NULL}
+    {"getDeviceList", getDeviceList, METH_VARARGS, "TODO"},
+    {NULL, NULL, 0, NULL}
 };
 
 PyMODINIT_FUNC init_scanning(void)
 {
-	(void) Py_InitModule("_scanning", scanningMethods);
-	
-	// mac os black magic: this is needed in order to get
-	//	garbage collection working properly
-	[[NSAutoreleasePool alloc] init];
-}
-
-int main (int argc, const char * argv[])
-{
+    (void) Py_InitModule("_scanning", scanningMethods);
+    
+    // mac os black magic: this is needed in order to get
+    //    garbage collection working properly
     [[NSAutoreleasePool alloc] init];
-	//NSDictionary *dict = getDevicePropertyDictionary(1073741827);
-	//getDeviceList();
-    return 0;
 }
